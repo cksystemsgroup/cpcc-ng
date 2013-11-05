1843ac54fc0a07d5de4eb264df83dc4480952950
Clemens Krainer <clemens.krainer@gmail.com>
2013-11-05 22:13:02 +0100

    Package name of QueryManager changed.

--- cpcc-persistence/src/main/java/at/uni_salzburg/cs/cpcc/persistence/services/QueryManager.java
+++ cpcc-persistence/src/main/java/at/uni_salzburg/cs/cpcc/persistence/services/QueryManager.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package at.uni_salzburg.cs.cpcc.persistence.db;
+package at.uni_salzburg.cs.cpcc.persistence.services;
 
 import java.util.Collection;
 import java.util.List;
