98bc47cd089836b413462339942b7269a59315c8
Clemens Krainer <clemens.krainer@gmail.com>
2017-02-07 22:05:06 +0100

    Some attributes added in SystemMonitorImpl

---
+++ cpcc-rv-base/src/main/java/cpcc/rv/base/services/MxBeanUtils.java
@@ -0,0 +1,171 @@
+// This code is part of the CPCC-NG project.
+//
+// Copyright (c) 2009-2017 Clemens Krainer <clemens.krainer@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+package cpcc.rv.base.services;
+
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.management.JMException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.TabularDataSupport;
+
+/**
+ * MxBean utility functions.
+ */
+public class MxBeanUtils
+{
+    public static final String PATH_SEPARATOR = "/";
+
+    private MBeanServer pmbs = ManagementFactory.getPlatformMBeanServer();
+
+    /**
+     * @param attributeName the attribute name.
+     * @return the attribute value.
+     * @throws JMException in case of errors.
+     */
+    @SuppressWarnings("rawtypes")
+    public String readMxBeanAttribute(String attributeName) throws JMException
+    {
+        String[] x = attributeName.split("(?<!\\\\)" + PATH_SEPARATOR);
+
+        if (x.length < 2)
+        {
+            throw new MalformedObjectNameException("Can not handle object " + attributeName);
+        }
+
+        x[0] = x[0].replaceAll("\\\\", "");
+        ObjectName on = new ObjectName(x[0]);
+        Object a = pmbs.getAttribute(on, x[1]);
+
+        if (x.length == 3)
+        {
+            CompositeDataSupport cd = (CompositeDataSupport) a;
+            return cd.get(x[2]).toString();
+        }
+
+        if ("[Ljava.lang.String;".equals(a.getClass().getName()))
+        {
+            return String.join(",", (String[]) a);
+        }
+
+        if ("javax.management.openmbean.TabularDataSupport".equals(a.getClass().getName()))
+        {
+            HashMap<String, String> m = new HashMap<String, String>();
+            TabularDataSupport data = (TabularDataSupport) a;
+            for (Iterator iter = data.keySet().iterator(); iter.hasNext();)
+            {
+                Object key = iter.next();
+                for (Iterator iter1 = ((List) key).iterator(); iter1.hasNext();)
+                {
+                    Object key1 = iter1.next();
+                    CompositeData valuedata = data.get(new Object[]{key1});
+                    String value = (String) valuedata.get("value");
+                    value = value.replaceAll("\n", "\\\\n").replaceAll("\r", "\\\\r").replaceAll("\t", "\\\\t");
+                    m.put(key1.toString(), value);
+                }
+            }
+
+            Object[] key = m.keySet().toArray();
+            Arrays.sort(key);
+
+            boolean first = true;
+            StringBuffer b = new StringBuffer();
+            for (int i = 0; i < key.length; i++)
+            {
+                if (!first)
+                {
+                    b.append("\t");
+                }
+                first = false;
+                b.append(key[i]).append('=').append(m.get(key[i]));
+            }
+
+            return b.toString();
+        }
+
+        return a.toString();
+    }
+
+    /**
+     * @return the list of available MxBean names.
+     */
+    @SuppressWarnings("rawtypes")
+    public List<String> listMxBeans()
+    {
+        ArrayList<String> beans = new ArrayList<String>();
+
+        Set mbNameSet = pmbs.queryNames(null, null);
+        Iterator mbNameSetIterator = mbNameSet.iterator();
+        while (mbNameSetIterator.hasNext())
+        {
+            Object o = mbNameSetIterator.next();
+            ObjectName on = (ObjectName) o;
+            try
+            {
+                MBeanInfo info = pmbs.getMBeanInfo(on);
+                MBeanAttributeInfo[] ai = info.getAttributes();
+                for (int k = 0, l = ai.length; k < l; k++)
+                {
+                    String s = o.toString();
+                    String attributeName =
+                        s.replaceAll(PATH_SEPARATOR, "\\\\" + PATH_SEPARATOR) + PATH_SEPARATOR + ai[k].getName();
+                    if ("javax.management.openmbean.CompositeData".equals(ai[k].getType()))
+                    {
+                        CompositeData cmp = (CompositeData) pmbs.getAttribute(on, ai[k].getName());
+                        if (cmp == null)
+                        {
+                            continue;
+                        }
+                        Set keySet = cmp.getCompositeType().keySet();
+                        Iterator cIterator = keySet.iterator();
+                        while (cIterator.hasNext())
+                        {
+                            Object n = cIterator.next();
+                            beans.add(attributeName + PATH_SEPARATOR + n.toString());
+                        }
+                    }
+                    else
+                    {
+                        beans.add(attributeName);
+                        String[] x = attributeName.split("(?<!\\\\)" + PATH_SEPARATOR);
+                        x[0] = x[0].replaceAll("\\\\", "");
+                    }
+                }
+            }
+            catch (JMException e)
+            {
+                // intentionally empty
+            }
+        }
+
+        return beans;
+    }
+
+}
--- cpcc-rv-base/src/main/java/cpcc/rv/base/services/SystemMonitorImpl.java
+++ cpcc-rv-base/src/main/java/cpcc/rv/base/services/SystemMonitorImpl.java
@@ -32,6 +32,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.management.JMException;
+
 import org.slf4j.Logger;
 
 /**
@@ -39,6 +41,19 @@ import org.slf4j.Logger;
  */
 public class SystemMonitorImpl implements SystemMonitor
 {
+    // private static final Set<String> ADDITIONAL_ATTRIBUTES = Collections.unmodifiableSet(Stream.of(
+    // "java.lang:type=OperatingSystem/ProcessCpuTime",
+    // "java.lang:type=OperatingSystem/ProcessCpuLoad",
+    // "java.lang:type=OperatingSystem/SystemCpuLoad",
+    // "java.lang:type=OperatingSystem/SystemLoadAverage",
+    // "java.lang:type=OperatingSystem/OpenFileDescriptorCount").collect(Collectors.toSet()));
+
+    private static final String PROCESS_CPU_TIME = "java.lang:type=OperatingSystem/ProcessCpuTime";
+    private static final String PROCESS_CPU_LOAD = "java.lang:type=OperatingSystem/ProcessCpuLoad";
+    private static final String SYSTEM_CPU_LOAD = "java.lang:type=OperatingSystem/SystemCpuLoad";
+    private static final String SYSTEM_LOAD_AVERAGE = "java.lang:type=OperatingSystem/SystemLoadAverage";
+    private static final String OPEN_FILE_COUNT = "java.lang:type=OperatingSystem/OpenFileDescriptorCount";
+
     private Runtime runtime;
     private MemoryMXBean memBean;
     private Logger logger;
@@ -48,6 +63,8 @@ public class SystemMonitorImpl implements SystemMonitor
     private RuntimeMXBean runtimeBean;
     private ThreadMXBean threadBean;
     private String header = "";
+    //    private List<String> attributes;
+    private MxBeanUtils mxb = new MxBeanUtils();
 
     /**
      * @param configuration the service configuration.
@@ -64,6 +81,10 @@ public class SystemMonitorImpl implements SystemMonitor
         runtimeBean = ManagementFactory.getRuntimeMXBean();
         threadBean = ManagementFactory.getThreadMXBean();
 
+        //        attributes = mxb.listMxBeans().stream()
+        //            .filter(x -> ADDITIONAL_ATTRIBUTES.contains(x))
+        //            .collect(Collectors.toList());
+
         writeStaticValues();
     }
 
@@ -78,11 +99,34 @@ public class SystemMonitorImpl implements SystemMonitor
             new SimpleEntry<String, Object>("processors", opsysBean.getAvailableProcessors()),
             new SimpleEntry<String, Object>("jvmName", runtimeBean.getName()),
             new SimpleEntry<String, Object>("jvmArch", opsysBean.getArch()),
-            new SimpleEntry<String, Object>("startTime", runtimeBean.getStartTime())
-            ).collect(Collectors.toList()));
+            new SimpleEntry<String, Object>("startTime", runtimeBean.getStartTime())).collect(Collectors.toList()));
 
         logger.info(";SH;" + entries.stream().map(x -> x.getKey().toString()).collect(Collectors.joining(";")));
         logger.info(";SV;" + entries.stream().map(x -> x.getValue().toString()).collect(Collectors.joining(";")));
+    }
+
+    private double readDoubleAttribute(String mxBeanAttributeName)
+    {
+        try
+        {
+            return Double.valueOf(mxb.readMxBeanAttribute(mxBeanAttributeName)).doubleValue();
+        }
+        catch (NumberFormatException | JMException e)
+        {
+            return Double.NaN;
+        }
+    }
+
+    private long readLongAttribute(String mxBeanAttributeName)
+    {
+        try
+        {
+            return Long.valueOf(mxb.readMxBeanAttribute(mxBeanAttributeName)).longValue();
+        }
+        catch (NumberFormatException | JMException e)
+        {
+            return 0L;
+        }
     }
 
     /**
@@ -91,11 +135,29 @@ public class SystemMonitorImpl implements SystemMonitor
     @Override
     public void writeLogEntry()
     {
-        List<SimpleEntry<String, Number>> entries = new ArrayList<>(Stream.of(
+        List<SimpleEntry<String, Number>> entries = new ArrayList<>();
+
+        entries.addAll(Stream.of(
             new SimpleEntry<String, Number>("time", System.currentTimeMillis()),
+            new SimpleEntry<String, Number>("uptime", runtimeBean.getUptime()),
+
+            new SimpleEntry<String, Number>("processCpuTime", readLongAttribute(PROCESS_CPU_TIME) / 1E6),
+            new SimpleEntry<String, Number>("processCpuLoad", readDoubleAttribute(PROCESS_CPU_LOAD)),
+
+            new SimpleEntry<String, Number>("systemCpuLoad", readDoubleAttribute(SYSTEM_CPU_LOAD)),
+            new SimpleEntry<String, Number>("systemLoadAvg", readDoubleAttribute(SYSTEM_LOAD_AVERAGE)),
+
+            new SimpleEntry<String, Number>("openFileCount", readLongAttribute(OPEN_FILE_COUNT)),
+
+            new SimpleEntry<String, Number>("threadCount", threadBean.getThreadCount()),
+            new SimpleEntry<String, Number>("peakThreadCount", threadBean.getPeakThreadCount()),
+            new SimpleEntry<String, Number>("daemonThreadCount", threadBean.getDaemonThreadCount()),
+            new SimpleEntry<String, Number>("totalStartedTheadCount", threadBean.getTotalStartedThreadCount()),
+
             new SimpleEntry<String, Number>("freeMemory", runtime.freeMemory()),
             new SimpleEntry<String, Number>("maxMemory", runtime.maxMemory()),
             new SimpleEntry<String, Number>("totalMemory", runtime.totalMemory()),
+
             new SimpleEntry<String, Number>("heap.init", memBean.getHeapMemoryUsage().getInit()),
             new SimpleEntry<String, Number>("heap.used", memBean.getHeapMemoryUsage().getUsed()),
             new SimpleEntry<String, Number>("heap.max", memBean.getHeapMemoryUsage().getMax()),
@@ -107,13 +169,8 @@ public class SystemMonitorImpl implements SystemMonitor
             new SimpleEntry<String, Number>("nonheap.committed", memBean.getNonHeapMemoryUsage().getCommitted()),
 
             new SimpleEntry<String, Number>("objPendingFinCount", memBean.getObjectPendingFinalizationCount()),
-            new SimpleEntry<String, Number>("sysLoadAvg", opsysBean.getSystemLoadAverage()),
-            new SimpleEntry<String, Number>("uptime", runtimeBean.getUptime()),
-            new SimpleEntry<String, Number>("threadCount", threadBean.getThreadCount()),
-            new SimpleEntry<String, Number>("peakThreadCount", threadBean.getPeakThreadCount()),
-            new SimpleEntry<String, Number>("daemonThreadCount", threadBean.getDaemonThreadCount()),
-            new SimpleEntry<String, Number>("totalStartedTheadCount", threadBean.getTotalStartedThreadCount())
-            ).collect(Collectors.toList()));
+            new SimpleEntry<String, Number>("sysLoadAvg", opsysBean.getSystemLoadAverage()))
+            .collect(Collectors.toList()));
 
         for (GarbageCollectorMXBean bean : gcBean)
         {
@@ -123,8 +180,8 @@ public class SystemMonitorImpl implements SystemMonitor
 
                 entries.addAll(Stream.of(
                     new SimpleEntry<String, Number>(prefix + ".gcTime", bean.getCollectionTime()),
-                    new SimpleEntry<String, Number>(prefix + ".gcCount", bean.getCollectionCount())
-                    ).collect(Collectors.toList()));
+                    new SimpleEntry<String, Number>(prefix + ".gcCount", bean.getCollectionCount()))
+                    .collect(Collectors.toList()));
             }
         }
 
@@ -138,8 +195,8 @@ public class SystemMonitorImpl implements SystemMonitor
                     new SimpleEntry<String, Number>(prefix + ".init", bean.getUsage().getInit()),
                     new SimpleEntry<String, Number>(prefix + ".max", bean.getUsage().getMax()),
                     new SimpleEntry<String, Number>(prefix + ".used", bean.getUsage().getUsed()),
-                    new SimpleEntry<String, Number>(prefix + ".commmitted", bean.getUsage().getCommitted())
-                    ).collect(Collectors.toList()));
+                    new SimpleEntry<String, Number>(prefix + ".commmitted", bean.getUsage().getCommitted()))
+                    .collect(Collectors.toList()));
             }
         }
 
